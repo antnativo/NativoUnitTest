55fe375c7422c8feee12d4d65e79566eb7f86056
antnativo <acrawford@nativo.net>
2016-05-10 15:56:20 -0700

    added new assertions to AddScript Test

--- public/tests/test-postrelease.js
+++ public/tests/test-postrelease.js
@@ -1,9 +1,11 @@
 QUnit.test("_pr.AddScript() : Add External JS File", function(assert){
   var done = assert.async();
   new _pr().AddScript("localhost:8081/mocks/loadscript.js");
+  assert.equal(jQuery("script[src='http://localhost:8081/mocks/loadscript.js']").length, 1, "JavaScript Tag is Added to Document");
+  assert.ok(jQuery("script[src='http://localhost:8081/mocks/loadscript.js']").parent()[0].isSameNode(jQuery("head")[0]), "JavaScript Tag is Added to Head");
   setTimeout(function(){
-    assert.ok(mock.isExternalScriptLoaded());
-      done();
+    assert.ok(mock.isExternalScriptLoaded(), "JavaScript was successfully loaded and executed");
+    done();
   },500);
   delete mock.isExternalScriptLoaded
 });
