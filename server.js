ea51bcc7f9e04b224dc5d33daf91f5be11ec68c9
antnativo <acrawford@nativo.net>
2016-05-10 11:48:35 -0700

    removed ssl and fs from server.js

--- server.js
+++ server.js
@@ -1,34 +1,6 @@
 var restify = require("restify");
-var fs = require("fs");
-
 
 //setup cors
-
-var server = restify.createServer();
-var https_options = {
-  key: fs.readFileSync('./self-signed/localhost.key'),
-  certificate: fs.readFileSync('./self-signed/localhost.crt')
-};
-var https_server =  restify.createServer(https_options);
-
-
-https_server.use(restify.acceptParser(server.acceptable));
-https_server.use(restify.authorizationParser());
-https_server.use(restify.dateParser());
-https_server.use(restify.queryParser());
-https_server.use(restify.jsonp());
-https_server.use(restify.gzipResponse());
-https_server.use(restify.bodyParser());
-https_server.use(restify.CORS());
-https_server.use(restify.fullResponse());
-https_server.opts(/.*/, function (req,res,next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
-    res.header("Access-Control-Allow-Headers", req.header("Access-Control-Request-Headers"));
-    res.send(200);
-    return next();
-});
-
 var server = restify.createServer();
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.authorizationParser());
@@ -39,6 +11,7 @@ server.use(restify.gzipResponse());
 server.use(restify.bodyParser());
 server.use(restify.CORS());
 server.use(restify.fullResponse());
+//setup cors
 server.opts(/.*/, function (req,res,next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
@@ -52,17 +25,6 @@ server.get(/(.*)/,restify.serveStatic({
 }));
 
 var port = process.env.PORT || 8080;
-server.listen(port,function(){
-	console.log("Server Started. Press Ctrl+c to quit server")
-})
-
-
-https_server.get(/(.*)/,restify.serveStatic({
-  directory: './public',
-  default: 'index.html'
-}));
-
-
-https_server.listen(443, function() {
-   console.log('%s listening at %s', https_server.name, https_server.url);
+server.listen(port, function () {
+  console.log("Server Started. Press Ctrl+c to quit server")
 });
